4b77509f4110ec04ad0b373b858e41c69ecb1d95
ShihPing0803 <119124535+ShihPing0803@users.noreply.github.com>
2024-12-19 23:40:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import os
 from langchain_openai import OpenAIEmbeddings
-from langchain.chains.vectorstores import Chroma
 from langchain.chains.question_answering import load_qa_chain
 from langchain.callbacks import get_openai_callback
 from openc import OpenCC
